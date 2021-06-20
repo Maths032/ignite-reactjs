c857ea53c846ed8da426621896fd94863a871db5
Matheus Ramos <matheus.ramos.silva.santos@gmail.com>
2021-06-20 20:41:21 -0300

    Include typescript

--- 01-gthub-explorer/src/Index.tsx
+++ 01-gthub-explorer/src/Index.tsx
@@ -1,4 +1,4 @@
-import './styles/global.scss'
+
 import { render } from 'react-dom'
 // import React from 'react'
 import { App } from './App'
---
+++ 01-gthub-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,35 @@
+import {RepositoryItem} from './RepositoryItem'
+
+import {useState, useEffect} from 'react'
+
+import '../styles/repositories.scss';
+const repository = {
+    name: 'unform',
+    description: 'Descrição do repositorio',
+    link: 'http://github.com/unform/unform' 
+}
+
+
+
+
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos').then((response)=>response.json()).then(data => setRepositories(data))
+    },[])
+
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                {repositories.map((repository) => {
+                    return (
+                      <RepositoryItem  key={repository.name} repository={repository} />
+                    )
+                })}
+            </ul>
+           
+        </section>
+    )
+}
